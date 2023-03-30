24bc75e92ae2ef707b16d86416969378cbe92f0c
santicano <canosantiago404@gmail.com>
2023-03-30 15:15:16 -0300

    Ejercicio 5 terminado

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    let total = 0;
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+
+    if (num1 < num2) {
+
+        for (let i = num1; i <= num2; i++) {
+            total += i;
+        }
+    
+        return total;  
+
+    } else {
+
+        for (let i = num2; i <= num1; i++) {
+            total += i;
+        }
+    
+        return total;  
+    }
+
+
 
 };
 
